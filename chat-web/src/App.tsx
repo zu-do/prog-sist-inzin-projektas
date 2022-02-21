0878a58564aa67aa4e6808b0feca51982dd11475
Paulius Skliaudys <paulius.skliaudys@ktu.edu>
2022-02-21 11:07:46 +0200

    Created and linked firebase with the project

--- chat-web/src/App.tsx
+++ chat-web/src/App.tsx
@@ -1,23 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCa9bz6v8yGfJHHktwx0hGvzf_NqOy6QY8",
+  authDomain: "chat-web-projektas.firebaseapp.com",
+  projectId: "chat-web-projektas",
+  storageBucket: "chat-web-projektas.appspot.com",
+  messagingSenderId: "966943128507",
+  appId: "1:966943128507:web:82eac40edf4b396bf3a29e"
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
